3007af6f814bfe879adcf1cb0056dae0f1345217
naomieab <39127576+naomieab@users.noreply.github.com>
2022-08-04 22:12:57 +0200

    (indentation)
    no change in comparison to previous commit

--- host/host.c
+++ host/host.c
@@ -24,34 +24,34 @@ extern uint32_t nr_haplotypes[NR_REGIONS]; //an array keeping number of haplotyp
 extern uint32_t nr_reads[NR_REGIONS]; //idem as haplotypes
 
 
- 
+
 int main() {
 	struct dpu_set_t set, dpu;
 	uint32_t nr_dpus, nr_ranks, each_dpu;
 	FILE* data_file = read_data("./../HaplotypeCaller/ActiveRegionsReadsHaplotypes.csv");
-  time_t start, end;
+	time_t start, end;
 
 
 	DPU_ASSERT(dpu_alloc(DPU_ALLOCATE_ALL, NULL, &set));
 	DPU_ASSERT(dpu_load(set, DPU_BINARY, NULL));
 	DPU_ASSERT(dpu_get_nr_dpus(set, &nr_dpus));
-  DPU_ASSERT(dpu_get_nr_ranks(set, &nr_ranks));
-  printf("Nr ranks =%d, and Nr dpus = %d\n", nr_ranks, nr_dpus);
-  
+	DPU_ASSERT(dpu_get_nr_ranks(set, &nr_ranks));
+	printf("Nr ranks =%d, and Nr dpus = %d\n", nr_ranks, nr_dpus);
+
 
 	//i is the iteration: if we have several rounds to process on a set of dpus, each iteration process a single round
 	for (int iteration = 1; iteration < (NR_REGIONS / nr_dpus) + 1; iteration++) {
-    time(&start);
+		time(&start);
 		populate_mram(set, nr_dpus, iteration);
-    time(&end);
-    printf("Populate MRAM in %ld\n", end-start);
-    
+		time(&end);
+		printf("Populate MRAM in %ld\n", end - start);
+
 		printf("Launch DPU iteration %d\n", iteration);
-    time(&start);
+		time(&start);
 		DPU_ASSERT(dpu_launch(set, DPU_SYNCHRONOUS));
-    time(&end);
-		printf("Finished DPU work: time required %ld\n", end-start);
-		 
+		time(&end);
+		printf("Finished DPU work: time required %ld\n", end - start);
+
 		DPU_FOREACH(set, dpu, each_dpu) {
 			DPU_ASSERT(dpu_prepare_xfer(dpu, &likelihoods[each_dpu * iteration]));
 		}
@@ -59,27 +59,27 @@ int main() {
 
 
 		DPU_FOREACH(set, dpu, each_dpu) {
-				DPU_ASSERT(dpu_prepare_xfer(dpu, &nb_cycles[each_dpu * iteration]));
+			DPU_ASSERT(dpu_prepare_xfer(dpu, &nb_cycles[each_dpu * iteration]));
 		}
 		DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_FROM_DPU, "nb_cycles", 0, sizeof(int), DPU_XFER_DEFAULT));
 
-    /*for (int i = 0; i < nr_dpus; i++) {
-			printf("\nREGION %d\n", i*iteration);
-			printf("Number of cycles = %d\n", nb_cycles[i*iteration]);
-			printf("Number of haplotypes = %d\n", nr_haplotypes[i*iteration]);
-			printf("Number of reads = %d\n", nr_reads[i*iteration]);
-			for (int j = 0; j < nr_haplotypes[i*iteration]; j++) {
-				for (int k = 0; k < nr_reads[i*iteration]; k++) {
-					printf("%d => %f | ", likelihoods[i*iteration][j][k], (double)likelihoods[i*iteration][j][k] / (double)ONE);
+		/*for (int i = 0; i < nr_dpus; i++) {
+				printf("\nREGION %d\n", i*iteration);
+				printf("Number of cycles = %d\n", nb_cycles[i*iteration]);
+				printf("Number of haplotypes = %d\n", nr_haplotypes[i*iteration]);
+				printf("Number of reads = %d\n", nr_reads[i*iteration]);
+				for (int j = 0; j < nr_haplotypes[i*iteration]; j++) {
+					for (int k = 0; k < nr_reads[i*iteration]; k++) {
+						printf("%d => %f | ", likelihoods[i*iteration][j][k], (double)likelihoods[i*iteration][j][k] / (double)ONE);
+					}
+					printf("\n");
 				}
-				printf("\n");
-			}
-			printf("\n\n\n");
-		}*/
-		
+				printf("\n\n\n");
+			}*/
+
 	}
 
 	free_mem(data_file);
 	DPU_ASSERT(dpu_free(set));
-	return 0; 
+	return 0;
 }
