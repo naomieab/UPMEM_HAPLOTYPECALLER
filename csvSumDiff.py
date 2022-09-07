7ce8fbbaf0747a281c6eaf24857a1762327e7e2a
amoisson <amoisson@upmem.com>
2022-09-07 13:40:31 +0200

    better csv comparison script output

--- csvSumDiff.py
+++ csvSumDiff.py
@@ -21,23 +21,27 @@ def pad_None(iterator):
 def main():
     file1 = sys.argv[1]
     file2 = sys.argv[2]
-    print("comparing "+file1+" and "+file2)
+    print("comparing "+file1+" and "+file2+"\n")
     total = 0
     total_sq = 0
     n = 0
     for v1, v2 in zip(pad_None(values(file1)), pad_None(values(file2))):
         if v1==None or v2==None:
             if v1!=None or v2!=None:
-                print("files aren't the same length")
+                print("\033[1;31mFiles aren't the same length\033[0m\n")
             break
         diff = v2-v1
         if diff*diff > 1:
-            print(v1,v2)
+            print("\033[31m"+(str(v1)+",").ljust(8)+"\t"+str(v2)+"\033[0m")
+        elif diff*diff > 0.1:
+            print("\033[33m"+(str(v1)+",").ljust(8)+"\t"+str(v2)+"\033[0m")
+        elif diff*diff > 0.001:
+            print((str(v1)+",").ljust(8)+"\t"+str(v2))
         total += diff
         total_sq += diff*diff
         n += 1
     variance = total_sq/n - total*total/n/n
-    print("total=", total)
+    print("\ntotal=", total)
     print("avg=", total/n)
     print("variance=", variance)
     print("std-dev=", sqrt(variance))
