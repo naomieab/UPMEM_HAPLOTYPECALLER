1a98fb6a5238bea4bc760f4de01dd7be2ca95827
naomieab <39127576+naomieab@users.noreply.github.com>
2022-12-01 08:31:38 +0100

    Add writing to output file

--- host/result_collector.c
+++ host/result_collector.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SUB_REGIONS 600
+
 /*
 * Collect results from dpu_result_queue
 * The results are written in the dpu_result_buffer
@@ -15,12 +15,14 @@ bool received_regions[TOTAL_REGIONS]; //keeps the regions received
 int sub_region_count[TOTAL_REGIONS]; //keeps the number of subregions received - the number of regions
 bool allocated_regions[TOTAL_REGIONS]; //bool whether the region was allocated in results
 int** results[TOTAL_REGIONS];
+int region_reads_nb[TOTAL_REGIONS];
+int region_haps_nb[TOTAL_REGIONS];
 
 
 void collect_result(FILE* output_file) {
 	//initialization
+	int next_to_write = 0;
 	for (int i = 0; i < TOTAL_REGIONS; i++) {
-		received_regions[i] = 0;
 		sub_region_count[i] = 0;
 		allocated_regions[i] = 0;
 	}
@@ -31,6 +33,8 @@ void collect_result(FILE* output_file) {
 			struct region_shape_t region = dpu_results.region_shapes[i];
 			//allocate results for region if needed
 			if (!allocated_regions[region.region_index]) {
+				region_reads_nb[region.region_index] = region.total_reads_region;
+				region_haps_nb[region.region_index] = region.total_haps_region;
 				results[region.region_index] = malloc(region.total_reads_region * sizeof(int*));
 				if (!results[region.region_index]) { printf("Error allocating\n"); }
 				for (int j = 0; j < region.total_reads_region; j++) {
@@ -49,10 +53,25 @@ void collect_result(FILE* output_file) {
 			if (region.last_subregion) {
 				sub_region_count[region.region_index] += region.total_nr_subregions;
 			}
+			if (sub_region_count[region.region_index] == 0) {
+				received_regions[region.region_index] = 1;
+			}
 		}
 		queue_release(&dpu_results_queue, current_result);
 		current_result = queue_take(&dpu_results_queue);
+		while (received_regions[next_to_write] == 1 && next_to_write < TOTAL_REGIONS) {
+			//write results to output and free mem
+			for (int i = 0; i < region_reads_nb[next_to_write]; i++) {
+				for (int j = 0; j < region_haps_nb[next_to_write]; j++) {
+					fprintf(output_file, "%f,", (double)results[next_to_write][i][j] / (double)ONE);
+				}
+				fprintf(output_file, "\n");
+				free(results[next_to_write][i]);
+			}
+			fprintf(output_file, "\n\n\n");
+			free(results[next_to_write]);
+			next_to_write++;
+		}
 	}
-
 }
 
