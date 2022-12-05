97c1d0fdcd7773945d6b03ceea63d6eea69ccf8c
amoisson <amoisson@upmem.com>
2022-12-05 15:27:46 +0100

    fixed async arguments problems

--- host/launch_dpus.c
+++ host/launch_dpus.c
@@ -12,6 +12,8 @@
 static struct region_shape_t empty_region_shape = {0,0,0};
 static int64_t dummy_likelihoods[MAX_READ_NUM][MAX_HAPLOTYPE_NUM];
 
+static pthread_mutex_t populating_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 dpu_error_t run_rank(struct dpu_set_t set, uint32_t rank_id, void* arg) {
 	uint32_t each_dpu;
 	struct dpu_set_t dpu;
@@ -19,11 +21,13 @@ dpu_error_t run_rank(struct dpu_set_t set, uint32_t rank_id, void* arg) {
 	// Reserve all needed regions for this rank.
 	bool all_dpus_inactive = true;
 	bool queue_closed = false;
+	pthread_mutex_lock(&populating_mutex);
 	DPU_FOREACH(set, dpu, each_dpu) {
 		int region_id;
 		if (queue_closed) {
 			region_id = -1;
 		} else {
+			LOG_DEBUG("taking from queue\n");
 			region_id = queue_take(&dpu_regions_queue);
 		}
 		dpu_buffer_indices[each_dpu] = region_id;
@@ -45,73 +49,10 @@ dpu_error_t run_rank(struct dpu_set_t set, uint32_t rank_id, void* arg) {
 		if (dpu_buffer_indices[each_dpu] < 0) {
 			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
 		} else {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].reads_len));
-		}
-	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_reads_len", 0, MAX_READ_NUM * sizeof(uint64_t), DPU_XFER_ASYNC));
-
-	DPU_FOREACH(set, dpu, each_dpu) {
-		if (dpu_buffer_indices[each_dpu] < 0) {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
-		} else {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].haplotypes_len));
-		}
-	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_haplotypes_len", 0, MAX_HAPLOTYPE_NUM * sizeof(uint64_t), DPU_XFER_ASYNC));
-
-	DPU_FOREACH(set, dpu, each_dpu) {
-		if (dpu_buffer_indices[each_dpu] < 0) {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
-		} else {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].reads_array));
-		}
-	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_reads_array", 0, MAX_READ_NUM * MAX_READ_LENGTH * sizeof(char), DPU_XFER_ASYNC));
-
-	DPU_FOREACH(set, dpu, each_dpu) {
-		if (dpu_buffer_indices[each_dpu] < 0) {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
-		} else {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].match_to_indel));
-		}
-	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_matchToIndelArray", 0, MAX_READ_NUM * MAX_READ_LENGTH * sizeof(int32_t), DPU_XFER_ASYNC));
-
-	DPU_FOREACH(set, dpu, each_dpu) {
-		if (dpu_buffer_indices[each_dpu] < 0) {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
-		} else {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].priors));
-		}
-	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_priors", 0, 2 * MAX_READ_NUM * MAX_READ_LENGTH * sizeof(int32_t), DPU_XFER_ASYNC));
-
-	DPU_FOREACH(set, dpu, each_dpu) {
-		if (dpu_buffer_indices[each_dpu] < 0) {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
-		} else {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].haplotypes_array));
-		}
-	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_haplotypes_array", 0, MAX_HAPLOTYPE_NUM * MAX_HAPLOTYPE_LENGTH * sizeof(char), DPU_XFER_ASYNC));
-
-	DPU_FOREACH(set, dpu, each_dpu) {
-		if (dpu_buffer_indices[each_dpu] < 0) {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
-		} else {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].haplotypes_val));
-		}
-	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_haplotypes_val", 0, MAX_HAPLOTYPE_NUM * sizeof(uint64_t), DPU_XFER_ASYNC));
-
-	DPU_FOREACH(set, dpu, each_dpu) {
-		if (dpu_buffer_indices[each_dpu] < 0) {
-			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
-		} else {
 			DPU_ASSERT(dpu_prepare_xfer(dpu, &dpu_regions_buffer[dpu_buffer_indices[each_dpu]].nr_haplotypes));
 		}
 	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "nr_haplotypes", 0, sizeof(uint64_t), DPU_XFER_ASYNC));
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "nr_haplotypes", 0, sizeof(uint64_t), DPU_XFER_DEFAULT));
 
 	DPU_FOREACH(set, dpu, each_dpu) {
 		if (dpu_buffer_indices[each_dpu] < 0) {
@@ -120,7 +61,7 @@ dpu_error_t run_rank(struct dpu_set_t set, uint32_t rank_id, void* arg) {
 			DPU_ASSERT(dpu_prepare_xfer(dpu, &dpu_regions_buffer[dpu_buffer_indices[each_dpu]].nr_reads));
 		}
 	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "nr_reads", 0, sizeof(uint64_t), DPU_XFER_ASYNC));
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "nr_reads", 0, sizeof(uint64_t), DPU_XFER_DEFAULT));
 
 	DPU_FOREACH(set, dpu, each_dpu) {
 		if (dpu_buffer_indices[each_dpu] < 0) {
@@ -129,7 +70,7 @@ dpu_error_t run_rank(struct dpu_set_t set, uint32_t rank_id, void* arg) {
 			DPU_ASSERT(dpu_prepare_xfer(dpu, &dpu_regions_buffer[dpu_buffer_indices[each_dpu]].read_region_starts));
 		}
 	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "read_region_starts", 0, (MAX_REGIONS_PER_DPU+1) * sizeof(uint32_t), DPU_XFER_ASYNC));
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "read_region_starts", 0, (MAX_REGIONS_PER_DPU+1) * sizeof(uint32_t), DPU_XFER_DEFAULT));
 
 	DPU_FOREACH(set, dpu, each_dpu) {
 		if (dpu_buffer_indices[each_dpu] < 0) {
@@ -138,18 +79,83 @@ dpu_error_t run_rank(struct dpu_set_t set, uint32_t rank_id, void* arg) {
 			DPU_ASSERT(dpu_prepare_xfer(dpu, &dpu_regions_buffer[dpu_buffer_indices[each_dpu]].haplotype_region_starts));
 		}
 	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "haplotype_region_starts", 0, (MAX_REGIONS_PER_DPU+1) * sizeof(uint32_t), DPU_XFER_ASYNC));
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "haplotype_region_starts", 0, (MAX_REGIONS_PER_DPU+1) * sizeof(uint32_t), DPU_XFER_DEFAULT));
+
+	DPU_FOREACH(set, dpu, each_dpu) {
+		if (dpu_buffer_indices[each_dpu] < 0) {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
+		} else {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].reads_len));
+		}
+	}
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_reads_len", 0, MAX_READ_NUM * sizeof(uint64_t), DPU_XFER_DEFAULT));
+
+	DPU_FOREACH(set, dpu, each_dpu) {
+		if (dpu_buffer_indices[each_dpu] < 0) {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
+		} else {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].haplotypes_len));
+		}
+	}
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_haplotypes_len", 0, MAX_HAPLOTYPE_NUM * sizeof(uint64_t), DPU_XFER_DEFAULT));
+
+	DPU_FOREACH(set, dpu, each_dpu) {
+		if (dpu_buffer_indices[each_dpu] < 0) {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
+		} else {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].reads_array));
+		}
+	}
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_reads_array", 0, MAX_READ_NUM * MAX_READ_LENGTH * sizeof(char), DPU_XFER_DEFAULT));
+
+	DPU_FOREACH(set, dpu, each_dpu) {
+		if (dpu_buffer_indices[each_dpu] < 0) {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
+		} else {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].match_to_indel));
+		}
+	}
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_matchToIndelArray", 0, MAX_READ_NUM * MAX_READ_LENGTH * sizeof(int32_t), DPU_XFER_DEFAULT));
+
+	DPU_FOREACH(set, dpu, each_dpu) {
+		if (dpu_buffer_indices[each_dpu] < 0) {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
+		} else {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].priors));
+		}
+	}
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_priors", 0, 2 * MAX_READ_NUM * MAX_READ_LENGTH * sizeof(int32_t), DPU_XFER_DEFAULT));
+
+	DPU_FOREACH(set, dpu, each_dpu) {
+		if (dpu_buffer_indices[each_dpu] < 0) {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
+		} else {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].haplotypes_array));
+		}
+	}
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_haplotypes_array", 0, MAX_HAPLOTYPE_NUM * MAX_HAPLOTYPE_LENGTH * sizeof(char), DPU_XFER_DEFAULT));
+
+	DPU_FOREACH(set, dpu, each_dpu) {
+		if (dpu_buffer_indices[each_dpu] < 0) {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, &priors));
+		} else {
+			DPU_ASSERT(dpu_prepare_xfer(dpu, dpu_regions_buffer[dpu_buffer_indices[each_dpu]].haplotypes_val));
+		}
+	}
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mram_haplotypes_val", 0, MAX_HAPLOTYPE_NUM * sizeof(uint64_t), DPU_XFER_DEFAULT));
+
 
 	// Ensure all data transfers are done before releasing it.
-	DPU_ASSERT(dpu_sync(set));
+	//LOG_DEBUG("dpu sync\n");
+	//DPU_ASSERT(dpu_sync(set));
 	// Release all regions sent.
 	DPU_FOREACH(set, dpu, each_dpu) {
 		queue_release(&dpu_regions_queue, dpu_buffer_indices[each_dpu]);
 	}
+	pthread_mutex_unlock(&populating_mutex);
 	
 	LOG_INFO("launching rank %d\n", rank_id);
-	DPU_ASSERT(dpu_launch(set, DPU_ASYNCHRONOUS));
-	DPU_ASSERT(dpu_sync(set));
+	DPU_ASSERT(dpu_launch(set, DPU_SYNCHRONOUS));
 
 	//Get results back
 	LOG_INFO("fetching results for rank %d\n", rank_id);
@@ -163,7 +169,7 @@ dpu_error_t run_rank(struct dpu_set_t set, uint32_t rank_id, void* arg) {
 			DPU_ASSERT(dpu_prepare_xfer(dpu, dummy_likelihoods));
 		}
 	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_FROM_DPU, "likelihoods", 0, MAX_HAPLOTYPE_NUM * MAX_READ_NUM * sizeof(int64_t), DPU_XFER_ASYNC));
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_FROM_DPU, "likelihoods", 0, MAX_HAPLOTYPE_NUM * MAX_READ_NUM * sizeof(int64_t), DPU_XFER_DEFAULT));
 
 	uint64_t dummy_nr_cycles;
 	DPU_FOREACH(set, dpu, each_dpu) {
@@ -173,7 +179,7 @@ dpu_error_t run_rank(struct dpu_set_t set, uint32_t rank_id, void* arg) {
 			DPU_ASSERT(dpu_prepare_xfer(dpu, &(dpu_results_buffer[dpu_buffer_indices[each_dpu]].nr_cycles)));
 		}
 	}
-	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_FROM_DPU, "nb_cycles", 0, sizeof(uint64_t), DPU_XFER_ASYNC));
+	DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_FROM_DPU, "nb_cycles", 0, sizeof(uint64_t), DPU_XFER_DEFAULT));
 
 	DPU_FOREACH(set, dpu, each_dpu) {
 		if (dpu_buffer_indices[each_dpu] >= 0) {
@@ -196,7 +202,7 @@ dpu_error_t run_rank(struct dpu_set_t set, uint32_t rank_id, void* arg) {
 		return DPU_OK;
 	}
 	// Callback itself
-	DPU_ASSERT(dpu_callback(set, run_rank, NULL, DPU_CALLBACK_ASYNC | DPU_CALLBACK_NONBLOCKING));
+	DPU_ASSERT(dpu_callback(set, run_rank, NULL, DPU_CALLBACK_ASYNC));
 	return DPU_OK;
 }
 
@@ -216,7 +222,7 @@ void launch_all_ranks() {
 		queue_open_writer(&dpu_results_queue);
 	}
 
-	DPU_ASSERT(dpu_callback(set_all_dpus, run_rank, NULL, DPU_CALLBACK_ASYNC | DPU_CALLBACK_NONBLOCKING));
+	DPU_ASSERT(dpu_callback(set_all_dpus, run_rank, NULL, DPU_CALLBACK_ASYNC));
 }
 
 void free_dpus() {
